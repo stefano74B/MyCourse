d3a27a279757fc7f0d14594cdd750d12702f186e
StefanoB74 <163399283+StefanoB74@users.noreply.github.com>
2024-10-31 21:14:26 +0100

    Aggiornata l'applicazione a NET.CORE 2.2

--- Startup.cs
+++ Startup.cs
@@ -16,7 +16,7 @@ namespace MyCourse
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
