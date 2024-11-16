4200c7596a40d4751e101f425fea4b74d6c26ea3
StefanoB74 <163399283+StefanoB74@users.noreply.github.com>
2024-11-16 14:26:49 +0100

    Creazione del primo model

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MyCourse.Models.Services.Application;
+using MyCourse.Models.ViewModels;
 
 namespace MyCourse.Controllers
 {
@@ -11,7 +13,9 @@ namespace MyCourse.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var courseService = new CourseService();
+            List<CourseViewModel> courses = courseService.GetServices();
+            return View(courses);
         }
 
         public IActionResult Detail(string id)
---
+++ Models/Services/Application/CourseService.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCourse.Models.Services.Application
+{
+    public class CourseService
+    {
+        
+        public List<CourseViewModel> GetServices()
+        {
+           throw new notImplementedException();  
+        }
+
+    }
+}
